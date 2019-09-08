f781c9b8920e4fbb625786dbe2435de22475e98e
ErpanArkin <erpan14ar@gmail.com>
2019-09-07 21:43:06 -0700

    add comments

--- stock_tools/position_sizing.py
+++ stock_tools/position_sizing.py
@@ -1,4 +1,9 @@
 
+'''
+This function gives the number of shares one should buy based on the net liquidation: net_cap,
+entry price, stop loss, and the risk per share which is default to 1%
+'''
+
 def position(net_cap, entry_price, stop_loss, risk_per_share = 0.01):
     shares = net_cap * risk_per_share / (entry_price - stop_loss)
     return round(shares)
