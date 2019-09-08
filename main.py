f4fba90565a253fe98532a6f84e4bd8ac6a78066
ErpanArkin <erpan14ar@gmail.com>
2019-09-07 21:57:30 -0700

    add the main script to run the program

---
+++ main.py
@@ -0,0 +1,5 @@
+from stock_tools.position_sizing import position
+
+net_cap = 1000; entry_price = 10.0; stop_loss = 9.80;
+
+print('number of shares are {}'.format(position(net_cap, entry_price, stop_loss)))
